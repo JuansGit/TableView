a23479ab1b811296cc26a05e0a82259eec91f8a2
Consultant <consultant@Consultants-MacBook-Pro.local>
2019-04-06 02:39:10 -0400

    First commit

---
+++ UPS/DeliveryOptions.swift
@@ -0,0 +1,13 @@
+//
+//  Options.swift
+//  UPS
+//
+//  Created by Consultant on 3/17/19.
+//  Copyright © 2019 JuanVitela. All rights reserved.
+//
+
+import Foundation
+
+class Options {
+    
+}
---
+++ UPS/DeliveryOptionsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DeliveryOptionsTableViewCell.swift
+//  UPS
+//
+//  Created by Consultant on 3/17/19.
+//  Copyright © 2019 JuanVitela. All rights reserved.
+//
+
+import UIKit
+
+class DeliveryOptionsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
